828a46f93cf6a628d55325c5797a0ed9f49adbe7
asquaremedia <31227202+asquaremedia@users.noreply.github.com>
2017-08-21 21:45:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -17,34 +17,6 @@ echo "<b>CPU Temp</b>";
 //cpu=$(</sys/class/thermal/thermal_zone0/temp);
 //echo "$((cpu/1000)) c";
 
-
-// Function to get the client ip address
-function get_client_ip_server() {
-    $ipaddress = '';
-    if ($_SERVER['HTTP_CLIENT_IP'])
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if($_SERVER['HTTP_X_FORWARDED'])
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if($_SERVER['HTTP_FORWARDED_FOR'])
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if($_SERVER['HTTP_FORWARDED'])
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if($_SERVER['REMOTE_ADDR'])
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
- 
-    return $ipaddress;
-}
-    
-echo get_client_ip_server();
-    
-$isp = geoip_isp_by_name('www.a-square-media.com');
-if ($isp) {
-    echo 'This host IP is from ISP: ' . $isp;
-}
     
 
 
