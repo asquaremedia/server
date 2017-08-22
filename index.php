964778538248edc6b5c05b5b8cde841ce0bc6420
asquaremedia <31227202+asquaremedia@users.noreply.github.com>
2017-08-21 21:42:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,30 @@ function get_client_ip_server() {
 }
     
 echo get_client_ip_server();
+    
+    // Function to get the client ip address
+function get_client_ip_env() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+ 
+    return $ipaddress;
+}
+
+echo get_client_ip_env();
+    
 
 
 echo '<button><a href="#?myip=$IP">Press Here</a></button>';
