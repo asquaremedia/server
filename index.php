b22c0a93306986cc1175a88c518adc26bd6dc016
asquaremedia <31227202+asquaremedia@users.noreply.github.com>
2017-08-21 21:40:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -37,20 +37,9 @@ function get_client_ip_server() {
         $ipaddress = 'UNKNOWN';
  
     return $ipaddress;
-}// Function to get the client ip address
-function get_client_ip_server() {
-    $ipaddress = '';
-    if ($_SERVER['HTTP_CLIENT_IP'])
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if($_SERVER['HTTP_X_FORWARDED'])
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if($_SERVER['HTTP_FORWARDED_FOR'])
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if($_SERVER['HTTP_FORWARDED'])
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if($_SERVER['REMOTE_ADDR']
+}
+    
+get_client_ip_server();
 
 
 echo '<button><a href="#?myip=$IP">Press Here</a></button>';
